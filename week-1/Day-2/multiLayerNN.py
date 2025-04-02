fae0c56ef3be6d8a0b729d690d9c95ea2f2e3845
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-04-02 22:36:48 +0530

    day3
    activation function,back propogation, 100days DL playlist

--- week-1/Day-2/multiLayerNN.py
+++ week-1/Day-2/multiLayerNN.py
@@ -26,14 +26,56 @@ class MultiLayer:
         return(z>0).astype(float)
     
     def forward(self,X):
+        print("\nForward Propogation Starts..")
         self.z1=np.dot(X,self.w1)+self.b1
         self.a1=self.relu(self.z1)
         self.z2=np.dot(self.a1,self.w2)+self.b2
         self.a2=self.sigmoid(self.z2)
+        print("\nForward Propogation ENDS..")
         return self.a2
     
     def backward(self,X,y):
+        print("\nBackward Propogation Starts..")
         m=X.shape[0]
+        dz2=self.a2-y
+        print("\ndz2.shape:",dz2.shape)
+        dw2=np.dot(self.a1.T,dz2)/m
+        print("\ndw2.shape:",dw2.shape)
+        db2=np.sum(dz2,axis=0,keepdims=True)/m
+        print("\ndb2.shape:",db2.shape)
+
+        da1=np.dot(dz2,self.w2.T)
+
+
+        
+
+        print("\nBackward Propogation ENDS..")
+        return True
+
+
+
+    def train(self,X_train,y_train,epochs=1000):
+        for i in range(epochs):
+            self.forward(X_train)
+            if i%100==0:
+                print("\nz1.shape:",self.z1.shape)
+                print("\na1.shape:",self.a1.shape)
+                print("\nz2.shape:",self.z2.shape)
+                print("\na2.shape:",self.a2.shape)
+            self.backward(X_train,y_train)
+            if i%100==0:
+                loss=np.mean((self.a2-y_train)**2)
+                print("\nEpoch:",i+1,"\t","loss:",loss)
+                print(f"\n-------------- epoch: {i} completed --------------\n")
+            break
+
+
+if __name__=='__main__':
+    X_train=np.random.rand(100,3)
+    y_train=np.random.randint(0,2,(100,1))
+    nn=MultiLayer(input_neuron=3,hidden_neuron=5,output_neuron=1,learning_rate=0.01)
+    nn.train(X_train,y_train,epochs=1000)
+    # print(y_train)
 
         
     
---
+++ week-1/Day-3/LossFunctions.py
@@ -0,0 +1,52 @@
+"""
+1) MSE - mean squared error
+2) MAE - mean absolute error
+3)
+
+
+    Task Type	                  Preferred Loss Function
+
+Regression	                 =   Mean Squared Error (MSE)
+Binary Classification	     =   Binary Cross-Entropy (BCE)
+Multiclass Classification	 =   Categorical Cross-Entropy (CCE)
+Imbalanced Classification	 =   Focal Loss (variant of BCE)
+
+"""
+import numpy as np
+
+
+
+# 1) MSE - primary goal is to predict continuous (regression) Values
+X=np.random.rand(10,3)
+print(X)
+
+weights=np.array([1,-0.27,1.88])
+print(weights)
+
+bias=2.873
+y_actual=X@weights+bias+np.random.randn(10)*0.01
+print(y_actual)
+
+predicted_weights=np.array([1.1,-0.11,1.78])
+predicted_bias=1.22
+y_predicted=X@predicted_weights+predicted_bias
+mse=np.mean((y_actual-y_predicted)**2)
+
+print("\nmse:",mse)
+
+
+
+# MAE - primary goal Regression Task
+"""
+advantage-  1) less sensitive to outlier as it take absolute of error
+            2) penalies all error equally, i.e. large or small error
+
+disadvantage-   1) Not differentiable at Zero
+                2) not useful when large error need to punish more than small errors!
+"""
+y_actual=np.array([3.2,5.8,8,9])
+y_predicted=np.array([3,5,8.9,8.7])
+
+mae = np.mean(np.abs(y_actual-y_predicted))
+print("\nMAE:",mae)
+
