026537ac5e10f2a728e48143cf9b7edc9c615be9
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-04-04 21:56:01 +0530

    Updated LossFunction.py

--- week-1/Day-3/LossFunctions.py
+++ week-1/Day-3/LossFunctions.py
@@ -31,11 +31,11 @@ predicted_bias=1.22
 y_predicted=X@predicted_weights+predicted_bias
 mse=np.mean((y_actual-y_predicted)**2)
 
-print("\nmse:",mse)
+print("\nMean Squared Error:",mse)
 
 
 
-# MAE - primary goal Regression Task
+# 2) MAE - primary goal Regression Task
 """
 advantage-  1) less sensitive to outlier as it take absolute of error
             2) penalies all error equally, i.e. large or small error
@@ -45,7 +45,63 @@ disadvantage-   1) Not differentiable at Zero
 """
 y_actual=np.array([3.2,5.8,8,9])
 y_predicted=np.array([3,5,8.9,8.7])
-
 mae = np.mean(np.abs(y_actual-y_predicted))
-print("\nMAE:",mae)
+print("Mean Absolute Error:",mae)
+
+
+# 3) huber loss
+def huber_loss(y_actual,y_predicted,delta=1):
+    """
+    combination of both MAE + MSE
+    useful when there are multiple outlier in data
+    """
+    errors=y_actual-y_predicted
+    loss=[]
+    for error in errors:
+        if abs(error)<=delta:
+            loss.append(0.5*error**2)
+        else:
+            loss.append(delta(abs(error)-(0.5*delta)))
+        return np.mean(loss)
+    
+y_actual=np.array([3.2,5.8,8,9])
+y_predicted=np.array([3,5,8.9,8.7])
+huberLoss=huber_loss(y_actual,y_predicted)
+print("Huber Loss:",huberLoss)
+
+
+# 4) Binary Cross Entropy / log loss
+"""
+Used for Binary Classifications !!
+"""
+def bce_loss(y_actual,y_predicted,epsilon=1e-12):
+    y_predicted=np.clip(y_predicted,epsilon,1.0-epsilon)
+    loss=-np.mean(y_actual*np.log(y_predicted)+(1-y_actual)*np.log(1-y_predicted))
+    return loss
+y_actual = np.array([1,0,1,1,0])
+y_predicted=np.array([0.7,0.9,0.3,0.55,0.14])
+binary_cross_entropy_loss=bce_loss(y_actual,y_predicted)
+print("binary_cross_entropy_loss:",binary_cross_entropy_loss)
+
+
+# 5) Categorical Class Entropy - CCE loss
+"""
+multiple class Prediction
+"""
+def cce_loss(y_actual,y_pred,epsilon=1e-12):
+    y_pred=np.clip(y_pred,epsilon,1-epsilon)
+    loss= -np.sum(y_actual*np.log(y_pred))/y_actual.shape[0]
+    return loss
+y_actual=np.array([
+    [1,0,0],
+    [0,1,0],
+    [0,0,1]
+])
+y_predicted=np.array([
+    [0.7,0.2,0.1],
+    [0.1,0.9,0.8],
+    [0.02,0.3,0.8]
+])
+categorical_class_entropy = cce_loss(y_actual,y_actual)
+print("categorical_class_entropy:",categorical_class_entropy)
 
