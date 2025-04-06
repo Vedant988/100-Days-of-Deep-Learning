7c44af7e5526970f404544b29347103c65e543c8
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-04-06 23:15:31 +0530

    Updated multiLayer.py

--- week-1/Day-2/multiLayerNN.py
+++ week-1/Day-2/multiLayerNN.py
@@ -26,60 +26,60 @@ class MultiLayer:
         return(z>0).astype(float)
     
     def forward(self,X):
-        print("\nForward Propogation Starts..")
         self.z1=np.dot(X,self.w1)+self.b1
         self.a1=self.relu(self.z1)
         self.z2=np.dot(self.a1,self.w2)+self.b2
         self.a2=self.sigmoid(self.z2)
-        print("\nForward Propogation ENDS..")
         return self.a2
     
     def backward(self,X,y):
-        print("\nBackward Propogation Starts..")
+        # print("\nBackward Propogation Starts..")
         m=X.shape[0]
         dz2=self.a2-y
-        print("\ndz2.shape:",dz2.shape)
+        # print("\ndz2.shape:",dz2.shape)
         dw2=np.dot(self.a1.T,dz2)/m
-        print("\ndw2.shape:",dw2.shape)
+        # print("\ndw2.shape:",dw2.shape)
         db2=np.sum(dz2,axis=0,keepdims=True)/m
-        print("\ndb2.shape:",db2.shape)
+        # print("\ndb2.shape:",db2.shape)
 
         da1=np.dot(dz2,self.w2.T)
+        dz1=da1*self.relu_derivative(self.z1)
+        dw1=np.dot(X.T,dz1)/m
+        db1=np.sum(dz1,axis=0,keepdims=True)/m
 
-
-        
-
-        print("\nBackward Propogation ENDS..")
+        self.w2 -= self.learning_rate*dw2
+        self.b2 -= self.learning_rate*db2
+        self.w1 -= self.learning_rate*dw1
+        self.b1 -= self.learning_rate*db1
+        # print("\nBackward Propogation ENDS..")
+        # print(f"\nw2: {self.w2}")
+        # print(f"b2: {self.b2}")
+        # print(f"w1: {self.w1}")
+        # print(f"b1: {self.b1}")
         return True
 
 
 
-    def train(self,X_train,y_train,epochs=1000):
+    def train(self,X_train,y_train,epochs):
         for i in range(epochs):
             self.forward(X_train)
-            if i%100==0:
-                print("\nz1.shape:",self.z1.shape)
-                print("\na1.shape:",self.a1.shape)
-                print("\nz2.shape:",self.z2.shape)
-                print("\na2.shape:",self.a2.shape)
+            # if i%100==0:
+            #     print("\nz1.shape:",self.z1.shape)
+            #     print("\na1.shape:",self.a1.shape)
+            #     print("\nz2.shape:",self.z2.shape)
+            #     print("\na2.shape:",self.a2.shape)
             self.backward(X_train,y_train)
             if i%100==0:
                 loss=np.mean((self.a2-y_train)**2)
                 print("\nEpoch:",i+1,"\t","loss:",loss)
                 print(f"\n-------------- epoch: {i} completed --------------\n")
-            break
+        return self.a2
 
 
 if __name__=='__main__':
     X_train=np.random.rand(100,3)
     y_train=np.random.randint(0,2,(100,1))
     nn=MultiLayer(input_neuron=3,hidden_neuron=5,output_neuron=1,learning_rate=0.01)
-    nn.train(X_train,y_train,epochs=1000)
+    output = nn.train(X_train,y_train,epochs=100000)
+    print(f"Output: {output[:5]}")
     # print(y_train)
-
-        
-    
-
-
-
-
