e974726d09b50653ef5f8cb4ed95980491d5b7a8
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-04-08 22:15:17 +0530

    Added file

--- week-1/Day-6/backprop_3in_4hid_1out.py
+++ week-1/Day-6/backprop_3in_4hid_1out.py
@@ -4,6 +4,29 @@ weights=np.random.randn(4,3)
 biases = np.array([-1.1,2.6,0.5,9.99])
 print(f'weights:{weights}')
 
+
+"""
+for understanding:
+
+forward propogagtion
+z1 = w11*X1 + w12*X2 + w13*X3 + b1
+z2 = w21*X1 + w22*X2 + w23*X3 + b2 
+z3 = w31*X1 + w32*X2 + w33*X3 + b3
+z4 = w41*X1 + w42*X2 + w43*X3 + b4
+
+activation function:
+a1 = relu(z1)
+a2 = relu(z2)
+a3 = relu(z3)
+a4 = relu(z4)
+
+output:
+y = a1+a2+a3+a4
+
+loss:
+loss = (y-0)^2
+
+"""
 learning_rate=0.001
 epochs=100
 
---
+++ week-1/Day-6/backprop_4in_3hid_1out.py
@@ -0,0 +1,17 @@
+"""
+for understanding:
+
+forward propogagtion
+z1 = w11*X1 + w12*X2 + w13*X3 + w14*X4 + b1
+z2 = w21*X1 + w22*X2 + w23*X3 + w24*X4 + b2 
+z3 = w31*X1 + w32*X2 + w33*X3 + w34*X4 + b3
+
+activation function output
+a1 = relu(z1)
+a2 = relu(z2)
+a3 = relu(z3)
+
+
+
+
+"""
