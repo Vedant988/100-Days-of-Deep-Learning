4f2067a0b5a314bcc8db5d505a01592cf84cb4cb
Vedant Badukale <149178970+Vedant988@users.noreply.github.com>
2025-04-27 23:12:31 +0530

    add

--- complete_nn.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-class Dense_layer:
-    def __init__(self,n_inputs,n_neurons):
-        self.weights = 0.01 * np.random.randn(n_inputs,n_neurons)
-        self.bias = np.zeros((1,n_neurons))
-        
